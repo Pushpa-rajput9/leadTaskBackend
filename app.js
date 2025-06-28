91b110e6dcb7b09d52716d0c7dab6216060c78d7
pushpa rajput <pr4541285@gmail.com>
2025-06-28 17:35:22 +0530

    Whitelist URLs

--- app.js
+++ app.js
@@ -7,7 +7,25 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://main.d1fpedi83xilbf.amplifyapp.com/",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
